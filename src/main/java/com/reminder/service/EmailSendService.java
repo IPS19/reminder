1511f635885b659bdd80b3d55f96cbf4d229f13b
IPS19 <i89214228815@gmail.com>
2026-04-25 20:20:02 +0300

    _

--- src/main/java/com/reminder/service/EmailSendService.java
+++ src/main/java/com/reminder/service/EmailSendService.java
@@ -11,7 +11,6 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class EmailSendService {
-
     private final JavaMailSender mailSender;
 
     @Value("${spring.mail.username}")
